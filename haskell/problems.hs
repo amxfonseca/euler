91fb41c8287334930a4a4e12ffb004becddcd7d6
André Fonseca <mail@andrefonseca.pt>
2019-10-23 21:12:32 +0200

    Adds solution to problem 4

--- haskell/problems.hs
+++ haskell/problems.hs
@@ -63,3 +63,26 @@ largestPrimeFactor_ n factor =
 
 largestPrimeFactor :: Int -> Int
 largestPrimeFactor n = largestPrimeFactor_ n 2
+
+-- Problem 4 - Largest palindrome product
+
+reverseNum_ :: Int -> Int -> Int
+reverseNum_ 0 sum = sum
+reverseNum_ n sum =
+  reverseNum_ quotient (sum * 10 + remainder)
+  where
+    (quotient, remainder) = divMod n 10
+
+reverseNum :: Int -> Int
+reverseNum = flip reverseNum_ 0
+
+isPalindrome :: Int -> Bool
+isPalindrome n = n == reverseNum n
+
+largestPalidromeFromProduct :: Int -> Int
+largestPalidromeFromProduct max =
+  maximum $
+    filter isPalindrome $
+    uncurry (*) <$> [ (k,l) | k <- range, l <- range, l >= k ]
+  where
+    range = [max, pred max..1]
