438c22fa250b8b03afed0983877400583c1969c2
André Fonseca <mail@andrefonseca.pt>
2019-10-24 16:42:06 +0200

    Make the code shorter and more efficient for P3
    It turns out that the smallest possible divisor of any number is always a prime. Which makes some sense after thinking about it.
    So no need to test for primality, just start from the lowest number and
    go up from there

--- haskell/Problem3.hs
+++ haskell/Problem3.hs
@@ -1,38 +1,13 @@
 -- Problem 3 - Largest prime factor
 module Problem3 where
 
-isqrt :: Int -> Int
-isqrt = floor . sqrt . fromIntegral
-
-isPrime_ :: Int -> Int -> Bool
-isPrime_ num divisor
-    | divisor < 2 = True
-    | mod num divisor == 0 = False
-    | otherwise = isPrime_ num (pred divisor)
-
-isPrime :: Int -> Bool
-isPrime n = isPrime_ n (isqrt n)
-
-nextPrime :: Int -> Int
-nextPrime n =
-  if isPrime next then
-    next
-  else
-    nextPrime next
-  where
-    next = succ n
-
 largestPrimeFactor_ :: Int -> Int -> Int
-largestPrimeFactor_ n factor =
-  if modN == 0 then
-    if isPrime n then
-      n
-    else
-      largestPrimeFactor_ quotient factor
-  else
-    largestPrimeFactor_ n (nextPrime factor)
+largestPrimeFactor_ n fact
+  | fact >= n = n
+  | modN == 0 = largestPrimeFactor_ quotient fact
+  | otherwise = largestPrimeFactor_ n (succ fact)
   where
-    (quotient, modN) = divMod n factor
+    (quotient, modN) = divMod n fact
 
 largestPrimeFactor :: Int -> Int
 largestPrimeFactor n = largestPrimeFactor_ n 2
